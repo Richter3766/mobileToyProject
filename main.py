cc461fec50872e6820d757fda3f293356a6d25a7
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2023-08-18 10:45:44 +0900

    auth test

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import firebase_admin
-from firebase_admin import auth
+from firebase_admin import auth, credentials
 from flask import Flask
 from diningServer.routes import routes_list
 from werkzeug.serving import WSGIRequestHandler
+
 WSGIRequestHandler.protocol_version = "HTTP/1.1"
 
 
@@ -18,9 +19,8 @@ def create_app():
     return app
 
 
-default_app = firebase_admin.initialize_app()
+cred = credentials.RefreshToken('path/to/refreshToken.json')
+default_app = firebase_admin.initialize_app(cred)
 
 app1 = create_app()
 app1.run('0.0.0.0', port=5000, debug=False)
-
-
